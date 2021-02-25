437471592f7711ef4a8c9ba70d6c904ca5eedfc8
davinakeane1 <davinakeane1@gmail.com>
2021-02-25 23:08:55 +0000

    Twelfth commit

--- While_loops.py
+++ While_loops.py
@@ -2,3 +2,12 @@ x = 1
 while x < 4 :
     print(x)
     x = x + 1
+
+# Initialize offset
+offset = 8
+
+# Code the while loop
+while offset != 0 :
+    print("correcting...")
+    offset = offset - 1
+    print(offset)
