b0e3bbf21dd5b12e751730b356fcde209aff3055
davinakeane1 <davinakeane1@gmail.com>
2021-02-25 20:32:37 +0000

    ninth commit

---
+++ Equality.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Comparison of booleans
+True == False
+
+# Comparison of integers
+-5 *15 != 75
+
+# Comparison of strings
+"pyscript" == "PyScript"
+
+# Compare a boolean with an integer
+True == 1
